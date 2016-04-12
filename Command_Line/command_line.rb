3182625737e4a16cd4e17a9e332feb3b585b3e4e
LegaTux <josmpena91@gmail.com>
2016-04-12 19:18:16 +0200

    Add flash exercise about SOLID

---
+++ Command_Line/command_line.rb
@@ -0,0 +1,40 @@
+class CommandLine
+  def initialize
+    @users = {"legatux" => "pass123"}
+    @text
+  end
+
+  def take_input
+    gets.chomp.downcase
+  end
+
+  def take_text
+    @text = gets.chomp
+  end
+
+  def word_counter
+    @text.split(" ").count
+  end
+
+  def login?(username, password)
+    @users[username] == password
+  end
+
+  def start
+    puts "username"
+    print " > "
+    username = take_input
+    puts "password"
+    print" > "
+    password = take_input
+    if login?(username, password)
+      puts "Write your text"
+      take_text
+      words = word_counter
+      puts "Your text have #{words} words"
+    else
+      puts "invalid credentials"
+      puts " bye"
+    end
+  end
+end
